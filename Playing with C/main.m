aa65b7e1b3504c9d476399570bda01011e2b3cce
Alexis Matherlee <alexismatherlee@gmail.com>
2013-10-03 12:06:16 -0700

    Initial Commit

---
+++ Playing with C/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Playing with C
+//
+//  Created by Alexis Matherlee on 10/3/13.
+//  Copyright (c) 2013 Alexis Matherlee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
